8b4eeed7178da2fbb90a5231c9f9c0020655541e
Isaac Aeshliman <iaeshliman@gmail.com>
2021-02-10 12:46:54 -0600

    Initial Commit

---
+++ CPUSchedulingSimulation/src/CPUBurst.java
@@ -0,0 +1,5 @@
+
+public class CPUBurst extends Burst
+{
+
+}
---
+++ CPUSchedulingSimulation/src/Process.java
@@ -0,0 +1,12 @@
+import java.util.Queue;
+
+public class Process
+{
+	private String state;
+	private Queue<Burst> bursts;
+	
+	public Process()
+	{
+		
+	}
+}
