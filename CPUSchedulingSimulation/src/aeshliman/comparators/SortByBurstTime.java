792f246a9e3d1ddf41def65a2ef16f84f26cc321
Isaac Aeshliman <iaeshliman@gmail.com>
2021-02-15 21:02:20 -0600

    Comparators package comments
    Added comments to the classes in the comparators package

--- CPUSchedulingSimulation/src/aeshliman/comparators/SortByBurstTime.java
+++ CPUSchedulingSimulation/src/aeshliman/comparators/SortByBurstTime.java
@@ -6,6 +6,7 @@ import aeshliman.structure.CustomProcess;
 
 public class SortByBurstTime implements Comparator<CustomProcess>
 {
+	// Sorts low burst time to high burst time
 	public int compare(CustomProcess process1, CustomProcess process2)
 	{
 		return process1.peekBurstTime() - process2.peekBurstTime();
--- CPUSchedulingSimulation/src/aeshliman/comparators/SortByPriority.java
+++ CPUSchedulingSimulation/src/aeshliman/comparators/SortByPriority.java
@@ -6,6 +6,7 @@ import aeshliman.structure.CustomProcess;
 
 public class SortByPriority implements Comparator<CustomProcess>
 {
+	// Sorts high priority to low (0 is high 9 is low)
 	public int compare(CustomProcess process1, CustomProcess process2)
 	{
 		return process1.getPriority() - process2.getPriority();
